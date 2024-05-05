5059ea53f9f15559b8ffbf9f92a126fec8f4266e
BnjMui <142273088+BnjMui@users.noreply.github.com>
2024-05-05 16:26:01 +0200

    Laget en "template" av dashboardkomponentet
    Ikke akkurat ferdigstilt, men et godt eksempel av html

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,39 @@
+export default function Dashboard(){
+    return (
+        <>
+        <h1>Recomendations for user1 and user2</h1>
+
+        <section>
+            <article>
+                <h3>Catch up!</h3>
+                <p>You have x movies in common on your wishlists.</p>
+                <article>
+                    <img src="placehold.co/300x300"/>
+                    <h3>movie1</h3>
+                </article>
+                <article>
+                    <img src="placehold.co/300x300"/>
+                    <h3>movie2</h3>
+                </article>
+            </article>
+            <article>
+                <h3>Go safe!</h3>
+                <p>You have x favourite movies in common</p>
+                <article>
+                    <img src="placehold.co/300x300"/>
+                    <h3>movie1</h3>
+                </article>
+            </article>
+            <article>
+                <h3>Explore!</h3>
+                <p>You have these genres in common. Chech out what movies to choose from</p>
+                <ul>
+                    <li>genre1</li>
+                    <li>genre2</li>
+                </ul>
+            </article>
+        </section>
+
+        </>
+    )
+}
