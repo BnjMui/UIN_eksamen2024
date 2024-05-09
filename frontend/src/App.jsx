19603e9b307901551145eaba40c8feb8f0c68161
martiniusm <martimob@hiof.no>
2024-05-09 14:51:21 +0200

    Begynt på Homepage
    Har begynt på component Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Homepage from './components/Homepage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Homepage />
     </>
   )
 }
---
+++ frontend/src/components/Homepage.jsx
@@ -0,0 +1,20 @@
+export default function Homepage() {
+  return (
+    <main>
+        <h2>Hei, USER XXX</h2>
+        <article>
+            <h4>Movies i would like to watch</h4>
+            <p>MOVIE XXX</p>
+        </article>
+        <article>
+            <p>You have these movies in your watchlist:</p>
+            <p>FILM XXX</p>
+        </article>
+        <article>
+            <h3>Im going to watch with...</h3>
+            <p>USER XXX</p>
+            <p>USER XXX</p>
+        </article>
+    </main>
+  )
+}
