ede01eedb2d346ba480b15c3370bc1f1ae6f050e
martiniusm <martimob@hiof.no>
2024-05-09 15:07:58 +0200

    Begynt på Header
    Begynt på component Header, uten noen form for funksjonalitet per nå

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+           <h2>What To See?</h2>
+           <button><h4>What do i want to watch?</h4></button>
+           <p>Scroll through generes</p>
+           <p>USER XXX</p>
+        </header>
+    )
+}
