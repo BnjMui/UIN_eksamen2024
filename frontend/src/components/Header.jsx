7d901f1e128ff7b97fa0de1599052312c08b58a7
martiniusm <martimob@hiof.no>
2024-05-12 17:17:12 +0200

    La til li på alle elementene i header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,12 +3,10 @@ export default function Header({loggedInUser}) {
         <header>
             <nav>
                 <ul>
-                    <li>
-                        <h2>What To See?</h2>
-                        <button><h4>What do i want to watch?</h4></button>
-                        <p>Scroll through generes</p>
-                        <p>{loggedInUser}</p>
-                    </li>
+                    <li><h2>What To See?</h2></li>
+                    <li><button><h4>What do i want to watch?</h4></button></li>
+                    <li><p>Scroll through generes</p></li>
+                    <li><p>{loggedInUser}</p></li>
                 </ul>
             </nav>
         </header>
