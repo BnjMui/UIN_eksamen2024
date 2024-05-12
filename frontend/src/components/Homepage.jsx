e5f153b8c30c2807218122091ede1b829fe3dda8
martiniusm <martimob@hiof.no>
2024-05-12 19:46:20 +0200

    Fjernet p tagger fra homepage komponent

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -4,16 +4,12 @@ export default function Homepage() {
         <h2>Hei, USER XXX</h2>
         <article>
             <h4>Movies i would like to watch</h4>
-            <p>MOVIE XXX</p>
         </article>
         <article>
             <p>You have these movies in your watchlist:</p>
-            <p>FILM XXX</p>
         </article>
         <article>
             <h3>Im going to watch with...</h3>
-            <p>USER XXX</p>
-            <p>USER XXX</p>
         </article>
     </main>
   )
