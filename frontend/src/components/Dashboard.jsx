2a027a1975313386c283ac3b99bdcf7257ed8580
BnjMui <142273088+BnjMui@users.noreply.github.com>
2024-05-13 15:14:33 +0200

    API nesten ferdig. Mangler linker til sjangere

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,31 +1,58 @@
 import { useEffect, useState } from "react"
-import { favoriteGenres } from "../../../backend/schemaTypes/favoriteGenres"
 
 export default function Dashboard({loggedInUser, user}){
-    //console.log(loggedInUser)
     const [amountFM, setAmountFM] = useState(0)
     const [amountWishlist, setAmountWishlist] = useState(0)
+    const [APIFM, setAPIFM] = useState([])
+    const [APIWL, setAPIWL] = useState([])
     
+    let GCommon = false
     //Mapping current users data. AKA favorite movies and wishlisted movies and favorite genre of logged in user.
     const currentUserWishlist = []
+    let WlMovieId = []
     user?.map(e => {
         if(e.username == loggedInUser) {
             e.wishlistedMovie !== null ? 
             e.wishlistedMovie.map(m => {
-                currentUserWishlist.push(m.movieTitle)
+                currentUserWishlist.push(m.movieId)
             }) : null
         }
     })
+    {user?.map((e) => {
+        if(e.username == "Martinius" && e.wishlistedMovie !== null) {
+            e.wishlistedMovie.map(e => {
+                if(currentUserWishlist.includes(e.movieId)) {
+                    WlMovieId.push(e.movieId)
+                }
+            })
+            
+        }
+
+    })} 
+    //Favorite movies
     const currentUserFM = []
+    let FmMovieId = []
     user?.map(e => {
         if(e.username == loggedInUser) {
             e.favoriteMovies !== null ?
             e.favoriteMovies.map(m => {
-                currentUserFM.push(m.movieTitle)
+                currentUserFM.push(m.movieId)
             }) : null
         }
     })
+    
+    {user?.map((e) => {
+        if(e.username == "Martinius" && e.favoriteMovies !== null) {
+          e.favoriteMovies.map(e => {
+                if(currentUserFM.includes(e.movieId)) {
+                    FmMovieId.push(e.movieId)
+                }
+            })
+        
+        }
+    })} 
 
+    //Favorite Genres
     const currentUserFG = []
     user?.map(e => {
         if(e.username == loggedInUser) {
@@ -37,16 +64,35 @@ export default function Dashboard({loggedInUser, user}){
         }
     })
     //Counters for how many items in common.
-    let letAmountWishlist = 0
-    let letAmountFM = 0
-    let WLcommon = false
-    let FMCommon = false
-    let GCommon = false
     useEffect(()=>{
-        setAmountFM(letAmountFM)
-        setAmountWishlist(letAmountWishlist)
-    },[letAmountFM, letAmountWishlist])
-    
+        fetchMovieById(FmMovieId, setAPIFM, setAmountFM)
+        fetchMovieById(WlMovieId, setAPIWL, setAmountWishlist)
+    },[])
+
+    //Function for API-reference
+
+    const fetchMovieById = async(e, API, Amount) => {
+        const url = `https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=${e}`
+        const options = {
+	        method: 'GET',
+	        headers: {
+		        'X-RapidAPI-Key': '83b786ce90mshb478f97f6df797cp1f84c1jsn1e5edab39268',
+		        'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+	        }
+        }
+
+    try {
+	    const response = await fetch(url, options)
+	    const result = await response.json()
+	    console.log(result)
+        
+        API(result.results)
+        Amount(result.entries)
+    } catch (error) {
+	    console.error(error)
+        }
+    }
+
     return (
         <>
         <h1>Recomendations for user1 and user2</h1>
@@ -55,53 +101,45 @@ export default function Dashboard({loggedInUser, user}){
             <article>
                 <h3>Catch up!</h3>
                 <p>You have {amountWishlist} movies in common on your wishlists.</p>
-                {user?.map((e, i) => {
-                    if(e.username !== loggedInUser && e.wishlistedMovie !== null) {
-                      return (<article key={i}>  {e.wishlistedMovie.map((e, i) => {
-                            if(currentUserWishlist.includes(e.movieTitle)) {
-                                letAmountWishlist ++
-                                WLcommon = true
-                               return <p key={i}>{e.movieTitle}</p>
-                            }
-                        })}
-                    </article>)
-                    }
-                })} {WLcommon == false ? <p>No common movies in wishlist.</p> : null}
+                {amountWishlist === 0 ? <p>No common movies in wishlist.</p> : null}
+                {APIWL?.map((e, i) => {
+                        return (
+                            <article key={i}>
+                                <img src={e.primaryImage.url} width={250} height={364}/>
+                                <p>{e.originalTitleText.text}</p>
+                            </article>
+                        )
+                    })} 
             </article>
             <article>
                 <h3>Go safe!</h3>
                 <p>You have {amountFM} favourite movies in common</p>
-                {user?.map((e, i) => {
-                    if(e.username == "Martinius" && e.favoriteMovies !== null) {
-                      return (<article key={i}>  {e.favoriteMovies.map((e, i) => {
-                            if(currentUserFM.includes(e.movieTitle)) {
-                                letAmountFM ++
-                                FMCommon = true
-                                console.log(amountFM)
-                               return <p key={i}>{e.movieTitle}</p>
-                            }else return null
-                        })}
-                    </article>)
-                    }
-                })} {FMCommon == false ? <p>No favorite movies in common.</p> : null}
+                    {amountFM === 0 ? <p>No favorite movies in common.</p> : null}
+                    {APIFM?.map((e, i) => {
+                        return (
+                            <article key={i}>
+                                <img src={e.primaryImage.url} width={250} height={364}/>
+                                <p>{e.originalTitleText.text}</p>
+                            </article>
+                        )
+                    })}
             </article>
             <article>
                 <h3>Explore!</h3>
                 <p>You have these genres in common. Chech out what movies to choose from</p>
-                {GCommon == false ? <p>No common genres found.</p> : null}
                 <ul>
                     {user?.map((e, i) => {
                         if(e.username !== loggedInUser && e.favoriteGenres !== null) {
-                            e.favoriteGenres.map((g, i) => {
+                            return (e.favoriteGenres.map((g, i) => {
                                 if(currentUserFG.includes(g.genre)){
                                     GCommon = true
-                                    console.log(g.genre)
                                     return <li key={i}>{g.genre}</li>
                                 }
-                            })
+                            }))
                         }
                     })}
                 </ul>
+                    {GCommon == false ? <p>No common genres found.</p> : null}
             </article>
         </section>
 
