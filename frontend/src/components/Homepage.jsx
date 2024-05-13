1f8fbde35a262e813b94796a06e1188983b17737
martiniusm <martimob@hiof.no>
2024-05-13 20:50:11 +0200

    Jobbet mer med homepage
    Jobber med å få skrevet ut bilder til filmer som er i ønskelisten

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,4 +1,43 @@
+import { useEffect, useState } from "react"
+
 export default function Homepage({loggedInUser, user}) {
+  const [wishList, setWishList] = useState([])
+  
+  let WlMovieId = []
+    user?.map(e => {
+        if(e.username == loggedInUser) {
+            e.wishlistedMovie !== null ? 
+            e.wishlistedMovie.map(m => {
+                WlMovieId.push(m.movieId)
+            }) : null
+        }
+    })
+  
+  const fetchMovieById = async(e) => {
+      const url = `https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=${e}`
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': '83b786ce90mshb478f97f6df797cp1f84c1jsn1e5edab39268',
+          'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+        }
+      }
+
+  try {
+    const response = await fetch(url, options)
+    const result = await response.json()
+    console.log(result)
+      
+      setWishList(result.results)
+  } catch (error) {
+    console.error(error)
+      }
+  }
+
+  useEffect(()=>{
+    fetchMovieById(WlMovieId)
+  })
+
   return (
     <main>
         <h2>Hello, {loggedInUser}</h2>
@@ -7,22 +46,20 @@ export default function Homepage({loggedInUser, user}) {
         </article>
         <article>
             <p>You have these movies in your watchlist:</p>
+            {wishList?.map((e, i) => {
+                        return (
+                            <article key={i}>
+                                <img src={e.primaryImage.url} width={250} height={364}/>
+                                <p>{e.originalTitleText.text}</p>
+                            </article>
+                        )
+                    })}
         </article>
         <article>
             <h3>Im going to watch with...</h3>
             {user?.map((data, i) => {
             if (data.username !== loggedInUser)
             return <p key={i} >{data.username}</p>
-          })}
-        </article>
-        <article>
-            {user?.map((data, i) => {
-            if (data.username === loggedInUser)
-            return <article key={i}>
-            {data.favoriteMovies.map((m, i) => {
-                return <h1 key={i}>{m.movieTitle}</h1>
-            })}
-          </article>
           })}
         </article>
     </main>
