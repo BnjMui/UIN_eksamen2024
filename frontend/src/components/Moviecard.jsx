2254a619bf112aff9ca40c540f2afed899bdd440
BnjMui <142273088+BnjMui@users.noreply.github.com>
2024-05-19 20:10:00 +0200

    Står nå at bilde ikke finnes hvis et moviecard ikke har bilde

--- frontend/src/components/Moviecard.jsx
+++ frontend/src/components/Moviecard.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
     return (
         <article className="moviecardContent">
         <Link to={`https://www.imdb.com/title/${movieId}/`} target="_blanc">
-            <img src={imgUrl} width={250} height={364}/>
+            {imgUrl ? <img src={imgUrl}/> : <p>This movie has no current poster</p>}
             <p>{titleText}</p>
         </Link>
         </article>
